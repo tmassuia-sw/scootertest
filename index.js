966e87b9edf201e21b68a8b2597e5a80dd66fc05
Thiago Massuia <tmassuia@switch.com.uy>
2020-08-17 11:43:52 -0300

    react-ble-plx

--- index.js
+++ index.js
@@ -5,7 +5,12 @@
 import {AppRegistry} from 'react-native';
 // import App from 'react-native-ble-manager/example/App';
 import App from './App';
-import App2 from './App2';
+
+
+import App3 from './App3';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+// try with react-native-ble-plx
+//AppRegistry.registerComponent(appName, () => App3);
